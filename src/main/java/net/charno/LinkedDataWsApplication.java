952bd120690dc9477a8222c7de228becb4e91e80
charno <michael@charno.net>
2016-09-03 21:33:13 +0100

    Initial commit

---
+++ src/main/java/net/charno/LinkedDataWsApplication.java
@@ -0,0 +1,12 @@
+package net.charno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedDataWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedDataWsApplication.class, args);
+	}
+}
